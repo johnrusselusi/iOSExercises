5b48ccf55881e0d0e3b00ca7154c6e61a1361219
John Russel Usi <j.usi@klab.com>
2014-10-14 16:06:46 +0800

    Added MVCPattern

---
+++ MVCPattern/MVCPattern/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVCPattern
+//
+//  Created by John Russel Usi on 10/14/14.
+//  Copyright (c) 2014 Klab Cyscorpions Training Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
