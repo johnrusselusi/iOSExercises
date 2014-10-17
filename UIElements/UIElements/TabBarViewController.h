1b25f222106b5da006a496eb9af35718ca9edbe2
John Russel Usi <j.usi@klab.com>
2014-10-17 10:20:16 +0800

    Added TabBarViewController

---
+++ UIElements/UIElements/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  UIElements
+//
+//  Created by John Russel Usi on 10/17/14.
+//  Copyright (c) 2014 Klab Cyscorpions Training Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
