0f3f052b67427b74142c30b67d95938336c6b253
John Russel Usi <j.usi@klab.com>
2014-10-17 14:22:07 +0800

    Added CustomDelegate

---
+++ CustomDelegate/CustomDelegateTests/CustomDelegateTests.m
@@ -0,0 +1,34 @@
+//
+//  CustomDelegateTests.m
+//  CustomDelegateTests
+//
+//  Created by John Russel Usi on 10/17/14.
+//  Copyright (c) 2014 Klab Cyscorpions Training Center. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomDelegateTests : XCTestCase
+
+@end
+
+@implementation CustomDelegateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
