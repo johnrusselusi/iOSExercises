a641e8fa06da992acdecb1951aa4f829ee7da3cb
John Russel Usi <j.usi@klab.com>
2014-10-17 17:57:16 +0800

    Added CoreLocation

---
+++ CoreLocation/CoreLocation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreLocation
+//
+//  Created by John Russel Usi on 10/17/14.
+//  Copyright (c) 2014 Klab Cyscorpions Training Center. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
